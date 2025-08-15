51e49c056fbf96ba47c9005c8a74a03b05d9cb57
InsaneBroccoli <InsaneBroccoli@users.noreply.github.com>
2025-08-15 11:51:04 -0500

    initial set up

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,11 +1,42 @@
-
-
-int switch_start;
-int switch_end;
-int switch_coupler;
+#define LED1 5
+#define LED2 13
+#define WAIT 50
 
 void setup()
 {
-    pinmode(switch_start, INPUT_PULLUP);
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    Serial.begin(9600);
+}
+
+enum States {
+    INITIAL,
+    LEDS
+};
+
+States state = States::INITIAL;
+
+
+void loop()
+{
+    switch (state)
+    {
+    case States::INITIAL:
+        Serial.println("\nInitialising");
+        state = States::LEDS;
+        Serial.println("Running");
+        break;
     
+    case States::LEDS:
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, LOW);
+        delay(WAIT);
+        digitalWrite(LED1, LOW);
+        digitalWrite(LED2, HIGH);
+        delay(WAIT);
+        break;
+    
+    default:
+        break;
+    }
 }
