4ecee4898201374ca0f6695bafcd3f8e39677cfc
InsaneBroccoli <InsaneBroccoli@users.noreply.github.com>
2025-08-15 14:58:05 -0500

    up and running

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,11 +1,16 @@
-#define LED1 5
 #define LED2 13
-#define WAIT 50
+#define MOTOR 5
+#define WAIT 100
+#define BUTTON_START 9
+#define BUTTON_END 8
+#define BUTTON_RETURNED 7
 
 void setup()
 {
-    pinMode(LED1, OUTPUT);
     pinMode(LED2, OUTPUT);
+    pinMode(BUTTON_START, INPUT_PULLUP);
+    pinMode(BUTTON_END, INPUT_PULLUP);
+    pinMode(BUTTON_RETURNED, INPUT_PULLUP);
     Serial.begin(9600);
 }
 
@@ -26,19 +31,43 @@ void loop()
     {
     case States::INITIAL:
         Serial.println("\nInitialising");
-        state = States::TESTING;
+        state = States::IDLE;
         Serial.println("Running");
         break;
     
-    case States::LEDS:
-        digitalWrite(LED1, HIGH);
+    case States::TESTING:
         digitalWrite(LED2, LOW);
         delay(WAIT);
-        digitalWrite(LED1, LOW);
         digitalWrite(LED2, HIGH);
         delay(WAIT);
         break;
     
+    case States::IDLE:
+        if (digitalRead(BUTTON_START) == LOW)
+        {
+            state = States::MOVE_TO_END;
+            digitalWrite(MOTOR, HIGH);
+        } else
+        {
+            digitalWrite(MOTOR, LOW);
+            delay(WAIT);
+            digitalWrite(MOTOR, HIGH);
+            delay(WAIT);
+        }        
+    break;
+    case States::MOVE_TO_END:
+        if (digitalRead(BUTTON_END) == LOW)
+        {
+            state = States::RETURN_TO_START;
+            digitalWrite(MOTOR, LOW);
+        }        
+    break;
+    case States::RETURN_TO_START:
+        if (digitalRead(BUTTON_RETURNED) == LOW)
+        {
+            state = States::IDLE;
+        }
+    break;
     default:
         break;
     }
