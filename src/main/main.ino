a6b332fc5d4da15cad171b765b53370f04c499ff
InsaneBroccoli <InsaneBroccoli@users.noreply.github.com>
2025-08-18 14:44:04 -0500

    Motor working

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,14 +1,21 @@
 #include <Servo.h>
 
+// Pins
 #define LED2 13
-#define MOTOR 5
-#define WAIT 100
-#define TIME_TO_DELIVER 3000
+#define MOTOR1 4
+#define MOTOR2 5
 #define BUTTON_START 9
 #define BUTTON_END 8
 #define BUTTON_RETURNED 7
+
+// Waiting periods
+#define WAIT 100
+#define TIME_TO_DELIVER 3000
+
+// Positioning
 #define INIT_POS 0
 #define DEPLOY_POS 90
+
 
 Servo myservo;
 
@@ -17,6 +24,8 @@ void setup()
     myservo.attach(3);
 
     pinMode(LED2, OUTPUT);
+    pinMode(MOTOR1, OUTPUT);
+    pinMode(MOTOR2, OUTPUT);
     pinMode(BUTTON_START, INPUT_PULLUP);
     pinMode(BUTTON_END, INPUT_PULLUP);
     pinMode(BUTTON_RETURNED, INPUT_PULLUP);
@@ -68,13 +77,15 @@ void loop()
         if (digitalRead(BUTTON_START) == LOW)
         {
             state = States::MOVE_TO_END;
-            digitalWrite(MOTOR, HIGH);
+
+            // activate motor cw;
+            digitalWrite(MOTOR1, HIGH);
+            digitalWrite(MOTOR2, LOW);
         } else
         {
-            digitalWrite(MOTOR, LOW);
-            delay(WAIT);
-            digitalWrite(MOTOR, HIGH);
-            delay(WAIT);
+            // Stop motor
+            digitalWrite(MOTOR1, HIGH);
+            digitalWrite(MOTOR2, HIGH);
         }
         break;
     case States::MOVE_TO_END:
@@ -82,8 +93,13 @@ void loop()
         {
             state = States::DELIVER_BALL;
 
-            pos = DEPLOY_POS;
+            // Stop motor
+            digitalWrite(MOTOR1, HIGH);
+            digitalWrite(MOTOR2, HIGH);
+
             delay(1000);
+            // eject
+            pos = DEPLOY_POS;
             myservo.write(pos);
         }        
         break;
@@ -91,7 +107,11 @@ void loop()
         delay(TIME_TO_DELIVER);
 
         state = States::RETURN_TO_START;
-        digitalWrite(MOTOR, LOW);
+
+        // activate motor ccw
+        digitalWrite(MOTOR1, LOW);
+        digitalWrite(MOTOR2, HIGH);
+
         pos = INIT_POS;
         myservo.write(pos);
         break;
@@ -99,6 +119,10 @@ void loop()
         if (digitalRead(BUTTON_RETURNED) == LOW)
         {
             state = States::IDLE;
+
+            // Stop motor
+            digitalWrite(MOTOR1, HIGH);
+            digitalWrite(MOTOR2, HIGH);
         }
         break;
     default:
